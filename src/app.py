ed723baf908b34c4b44b1bc81d9ef4b08b89f0a3
Aung Khant Maw <aungkhantmaw64@gmail.com>
2023-12-17 23:15:32 +0700

    Create initial application

--- src/app.py
+++ src/app.py
@@ -1,33 +1,20 @@
 import dash
-from dash import html, dcc
+from dash import html
+import dash_bootstrap_components as dbc
+from plotly import express
 
-app = dash.Dash(__name__)
+from views import home
+
+app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
 
 app.layout = html.Div(
-    children = [
-       html.H1(children = "CardiacSense",
-               style={"textAlign":"center", 
-                      "padding":10,"flex":1}),
-        html.Div(
-           children=[
-               html.Label("Database"),
-               dcc.Dropdown(['DatabaseA', 'DatabaseB', 'DatabaseC'], 'DatabaseA', id='abc-dropdown'),
-               html.Br(),
-               html.Label("Record"),
-               dcc.Dropdown(['Record001','Record002','Record003'], 'Record001')
-           ],
-           style={"padding":10,"flex":1}
-       ),
-       html.Div
-       (
-           children=[
-               dcc.Graph()
-           ],
-           style={"padding":10,"flex":1}
-       )    
-    ],
-    style = {"display":"flex","flexDirection":"column"}
+    children=[home.header,
+              home.signal_view],
+    style={'display': 'flex',
+           'flexDirection': 'column'}
 )
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=8050, debug=True)
+    app.run(host="0.0.0.0",
+            port=8050,
+            debug=True)
